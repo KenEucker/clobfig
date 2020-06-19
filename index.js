3d8d2a089c1ef31785fd57f9d967548b19f003c2
Ken Eucker <keneucker@gmail.com>
2020-06-18 21:35:44 -0700

    Added the _configSelectors object for making the clobfiguration more readable.

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ const _configFilePathRelative = path.join(__dirname, '../config/')
 const _configFilePathAppRoot = path.join(appRoot.path, '/config/')
 const _configFilePath = fs.existsSync(_configFilePathRelative) ? _configFilePathRelative : _configFilePathAppRoot
 
-/// Get all of the files in the configuration folder
-const _configFiles = fs.readdirSync(_configFilePath)
+/// include files that match this in their filename (including extension)
+const _configSelectors = [ 'config.js', '.json' ]
+
+/// Get all of the files in the configuration folder that match the selectors
+const _configFiles = fs.readdirSync(_configFilePath).filter(filename => _configSelectors.reduce(s => filename.indexOf(s), false) )
 
 /// clobber all of the files matching with 'config.js' in the filename together
-const _config = _configFiles.reduce((out, configFilename) => configFilename.indexOf('config.js') !== -1 ? merge(out, require(path.join(_configFilePath, configFilename))) : out, { _configFilePath, _configFiles })
+const _config = _configFiles.reduce((out, configFilename) =>  merge(out, require(path.join(_configFilePath, configFilename))), { _configFilePath, _configFiles })
 
 /// Return the clobfiguration
 module.exports = _config
 module.exports._configFilePath = _configFilePath
 module.exports._configFiles = _configFiles
+module.exports._configSelectors = _configSelectors
