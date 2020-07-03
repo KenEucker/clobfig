48026eaee1810d11be5647a009c7c9d0a0a13f95
Ken Eucker <keneucker@gmail.com>
2020-07-03 11:08:06 -0700

    Updates to the determination of the appRootPath.

--- index.js
+++ index.js
@@ -8,98 +8,97 @@ const appRoot = require('app-root-path')
 
 class Clobfig {
 
-    constructor(configFolderName = 'config', configSelectors = [ 'config.js' ], dataSelectors = [ '.json' ]) {
+	constructor(configFolderName = 'config', configSelectors = ['config.js'], dataSelectors = ['.json']) {
 
-        /// Determine root configuration folder
-        this._configFilePathRelative = path.join(__dirname, `../${configFolderName}`)
-        this._configFilePathAppRoot = path.join(appRoot.path, configFolderName)
+		/// Determine root configuration folder
+		this._configFilePathRelative = path.join(__dirname, `../${configFolderName}`)
+		this._configFilePathAppRoot = path.dirname(require.main.filename || process.mainModule.filename)
+		this._configFilePathAppRoot = fs.existsSync(this._configFilePathAppRoot) ? path.join(this._configFilePathAppRoot, configFolderName) : path.join(appRoot.path, configFolderName)
 
-        /// include files that match this in their filename (including extension)
-        this._configSelectors = configSelectors
-        this._dataSelectors = dataSelectors
-        
-        const relativePathExists = fs.existsSync(this._configFilePathRelative)
-        const appRootPathExists = !relativePathExists ? fs.existsSync(this._configFilePathAppRoot) : false
+		/// include files that match this in their filename (including extension)
+		this._configSelectors = configSelectors
+		this._dataSelectors = dataSelectors
 
-        if (!relativePathExists && !appRootPathExists) {
-            return
-        }
+		const relativePathExists = fs.existsSync(this._configFilePathRelative)
+		const appRootPathExists = !relativePathExists ? fs.existsSync(this._configFilePathAppRoot) : false
 
-        const configFilePath = path.resolve(relativePathExists ? this._configFilePathRelative : this._configFilePathAppRoot)
+		if (!relativePathExists && !appRootPathExists) {
+			return
+		}
 
-        this.getConfig(configFilePath)
+		const configFilePath = path.resolve(relativePathExists ? this._configFilePathRelative : this._configFilePathAppRoot)
 
-    }
+		this.getConfig(configFilePath)
 
-    getConfigurationFilesData(configFilePath) {
+	}
 
-        /// Set the new config folder
-        this._configFilePath = !!configFilePath ? configFilePath : this._configFilePath
+	getConfigurationFilesData(configFilePath) {
 
-        /// Get all of the config files in the configuration folder
-        const allFilesInConfigFolder = fs.readdirSync(this._configFilePath)
-        const configFiles = []
-        const dataFilesAdded = {}
+		/// Set the new config folder
+		this._configFilePath = !!configFilePath ? configFilePath : this._configFilePath
 
-        const filterConfigFiles = (filename) => this._configSelectors.reduce((o, s) => o || filename.indexOf(s) !== -1, false)
-        const filterDataFiles = (filename) => this._dataSelectors.reduce((o, s) => o || (filename !== 'config.json' && filename.indexOf(s) !== -1), false)
-        const addEachDataFile = (dataFilename) => dataFilesAdded[dataFilename.replace('.json', '')] = require(path.join(this._configFilePath, dataFilename))
-        const clobber = (out, configFilename) =>  merge(out, require(path.join(this._configFilePath, configFilename)))
-        const reorderConfigFiles = (configFilename) => configFilename.indexOf('config.json') !==-1 && configFiles.length ? configFiles.unshift(configFilename) : configFiles.push(configFilename)
+		/// Get all of the config files in the configuration folder
+		const allFilesInConfigFolder = fs.readdirSync(this._configFilePath)
+		const configFiles = []
+		const dataFilesAdded = {}
 
-        /// Get all of the config files in the configuration folder that match these selectors to be clobbed together
-        this._configFiles = allFilesInConfigFolder.filter(filterConfigFiles)
-        /// Add the data from each of the data files to the clobfig object (ex: pages.json => clobfig.pages)
-        this._configFiles.forEach(reorderConfigFiles)
+		const filterConfigFiles = (filename) => this._configSelectors.reduce((o, s) => o || filename.indexOf(s) !== -1, false)
+		const filterDataFiles = (filename) => this._dataSelectors.reduce((o, s) => o || (filename !== 'config.json' && filename.indexOf(s) !== -1), false)
+		const addEachDataFile = (dataFilename) => dataFilesAdded[dataFilename.replace('.json', '')] = require(path.join(this._configFilePath, dataFilename))
+		const clobber = (out, configFilename) => merge(out, require(path.join(this._configFilePath, configFilename)))
+		const reorderConfigFiles = (configFilename) => configFilename.indexOf('config.json') !== -1 && configFiles.length ? configFiles.unshift(configFilename) : configFiles.push(configFilename)
 
-        /// Get all of the json data files in the configuration folder that match these selectors to be added to the config under the name of the json file
-        this._dataFiles = allFilesInConfigFolder.filter(filterDataFiles)
-        /// Add the data from each of the data files to the clobfig object (ex: pages.json => clobfig.pages)
-        this._dataFiles.forEach(addEachDataFile)
+		/// Get all of the config files in the configuration folder that match these selectors to be clobbed together
+		this._configFiles = allFilesInConfigFolder.filter(filterConfigFiles)
+		/// Add the data from each of the data files to the clobfig object (ex: pages.json => clobfig.pages)
+		this._configFiles.forEach(reorderConfigFiles)
 
-        console.log({dataFilesAdded, configFiles})
+		/// Get all of the json data files in the configuration folder that match these selectors to be added to the config under the name of the json file
+		this._dataFiles = allFilesInConfigFolder.filter(filterDataFiles)
+		/// Add the data from each of the data files to the clobfig object (ex: pages.json => clobfig.pages)
+		this._dataFiles.forEach(addEachDataFile)
 
-        /// clobber all of the files matching with 'config.js' in the filename together, starting with the added objects
-        this.config = merge(dataFilesAdded, configFiles.reduce(clobber, { }))
+		/// clobber all of the files matching with 'config.js' in the filename together, starting with the added objects
+		this.config = merge(dataFilesAdded, configFiles.reduce(clobber, {}))
 
-    }
+	}
 
-    getConfig(configFilePath) {
+	getConfig(configFilePath) {
 
-        if (!!configFilePath) {
-            this.getConfigurationFilesData(configFilePath)
-        }
+		if (!!configFilePath) {
+			this.getConfigurationFilesData(configFilePath)
+		}
 
-        if (!this.config) {
-            return {}
-        }
+		if (!this.config) {
+			return {}
+		}
 
-        const _exports = this.config
-        _exports._configFilePathRelative = this._configFilePathRelative
-        _exports._configFilePathAppRoot = this._configFilePathAppRoot
-        _exports._configFilePath = this._configFilePath
-        _exports._configSelectors = this._configSelectors
-        _exports._configFiles = this._configFiles
-        _exports._dataSelectors = this._dataSelectors
-        _exports._dataFiles = this._dataFiles
+		const _exports = this.config
+		_exports._configFilePathRelative = this._configFilePathRelative
+		_exports._configFilePathAppRoot = this._configFilePathAppRoot
+		_exports._configFilePath = this._configFilePath
+		_exports._configSelectors = this._configSelectors
+		_exports._configFiles = this._configFiles
+		_exports._dataSelectors = this._dataSelectors
+		_exports._dataFiles = this._dataFiles
 
-        return _exports
+		return _exports
 
-    }
+	}
 
 }
 
 const ClobfigFactory = (configFolderName) => {
 
-    this.Clobfig = Clobfig
+	this.Clobfig = Clobfig
 
-    const clobfig = new Clobfig(configFolderName)
+	const clobfig = new Clobfig(configFolderName)
 
-    const config = clobfig.getConfig()
+	const config = clobfig.getConfig()
 
-    config.Clobfig = Clobfig
+	config.Clobfig = Clobfig
 
-    return config
+	return config
 
 }
 
